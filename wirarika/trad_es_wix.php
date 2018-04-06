7a33328d38412df696c25c8d7aeb8a0707ce9b9e
DE MATOS Alexis <alexis.de@tal-wirarika.tal>
2018-04-06 11:36:57 +0200

    suppr

--- wirarika/trad_es_wix.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$phrase = ($_POST['text']);
-
-$command = escapeshellcmd('/var/www/cgi-bin/client_es_wix.py "'.$phrase.'"');
-$output = shell_exec($command);
-echo $output;
-
-?>
--- wirarika/trad_wix_es.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$phrase = ($_POST['text']);
-
-$command = escapeshellcmd('/var/www/cgi-bin/client_wix_es.py "'.$phrase.'"');
-$output = shell_exec($command);
-echo $output;
-
-?>
